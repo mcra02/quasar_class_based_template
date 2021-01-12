fb48b459b68d8cc72ce4872f878781c9ff1a3c39
mcra02 <maicolcrodrigoa@gmail.com>
2021-01-11 19:20:32 -0500

    initial template

---
+++ {{cookiecutter.component_name}}/{{cookiecutter.component_file_name}}.ts
@@ -0,0 +1,6 @@
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class {{cookiecutter.component_file_name}} extends Vue {
+  
+}
---
+++ {{cookiecutter.component_name}}/{{cookiecutter.component_file_name}}.vue
@@ -0,0 +1,9 @@
+<template>
+<div>
+  {{cookiecutter.component_file_name}}
+</div>
+</template>
+
+<script lang="ts" src="./{{cookiecutter.component_file_name}}.ts" />
+
+<style scoped lang="scss" src="./{{cookiecutter.component_file_name}}.scss" />
